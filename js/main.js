d9003295cb7ab35496168666627549ee96228a8e
Анна Радько <72429604+AnnaRadko163@users.noreply.github.com>
2024-07-10 00:45:45 +0400

    adding block price and questions

--- js/main.js
+++ js/main.js
@@ -10,70 +10,113 @@
     });
 // end header
 
-// start employees
-// const employeesSlider = document.querySelector('.employees__swiper');
-// if(employeesSlider){
-//   var aboutusSlider = new Swiper('.employees__swiper', {
-//     loop: true,
-//     slidesPerView: 4,
-//     loopedSlides: 4,
-//     spaceBetween: 20,
-//     speed: 500,
-//     navigation: {
-//       nextEl: '.wiper-button-prevt',
-//       prevEl: '.swiper-button-next',
-//     },
-//     breakpoints: {
-//       // 1919: {
-//       //   spaceBetween: 30,
-//       // },
-//       // 1023: {
-//       //   slidesPerView: 'auto',
-//       //   spaceBetween: 20,
-//       // },
-//     },
-//   });
-// }
+// start reviewsSlider
 
 const reviewsSlider = document.querySelector('.reviews__swiper');
 if(reviewsSlider){
-    var aboutusSlider = new Swiper('.reviews__swiper', {
-    // Optional parameters
+    var reviewSlider = new Swiper('.reviews__swiper', {
     loop: true,
-    slidesPerView: 4,
-    // sliderPerGroup: 4,
-    // loopedSlides: 4,
-    spaceBetween: 60,
+    slidesPerView: 1.2,
+    spaceBetween: 14,
     speed: 500,
-    slidesPerGroup: 4,
-  
-    // If we need pagination
+    slidesPerGroup: 1,
+    centeredSlides: true,
     pagination: {
-      el: '.swiper-pagination',
-      type:'fraction',
-      formatFractionCurrent: addZero,
-      formatFractionTotal: addZero
-
+      el: '.reviews__pagination_min',
+      clickable: true,
     },
-  
-    // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.reviews__next',
+      prevEl: '.reviews__prev',
     },
     breakpoints: {
-      1600: {
-        // slidesPerView: 4,
-        spaceBetween: 32,
-
-     },
-    }
-
-  
+      530: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+          centeredSlides: false,
+          spaceBetween: 20,
+          pagination: {
+            el: '.reviews__pagination',
+            type:'fraction',
+            formatFractionCurrent: addZero,
+            formatFractionTotal: addZero
+          },
+        },
+      768:{
+        slidesPerView: 3,
+        slidesPerGroup: 32,
+        centeredSlides: false,
+        pagination: {
+          el: '.reviews__pagination',
+          type:'fraction',
+          formatFractionCurrent: addZero,
+          formatFractionTotal: addZero
+        },
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 60,
+        centeredSlides: false,
+        slidesPerGroup: 4,
+        pagination: {
+          el: '.reviews__pagination',
+          type:'fraction',
+          formatFractionCurrent: addZero,
+          formatFractionTotal: addZero
+          },
+        }
+      }
   });
 }
+// end reviewsSlider
+
+
+// start priceSlider
+
+const priceSlider = document.querySelector('.price__swiper');
+if(priceSlider){
+    var pricesSlider = new Swiper('.price__swiper', {
+    loop: true,
+    speed: 500,
+    slidesPerView: 2,
+    spaceBetween: 40,
+    slidesPerGroup: 2,
+        
+    // pagination: {
+    //   el: '.price__pagination_min',
+    //   clickable: true,
+    // },
+    pagination: {
+          el: '.price__pagination',
+          type:'fraction',
+          formatFractionCurrent: addZero,
+          formatFractionTotal: addZero
+          },
+    navigation: {
+      nextEl: '.price__next',
+      prevEl: '.price__prev',
+    },
+    breakpoints: {
+      1024: {
+       
+        }
+      }
+  });
+}
+// end reviewsSlider
 
 function addZero(num){
-    return (num > 9) ? num : '0' + num;reviews__item
+    return (num > 9) ? num : '0' + num
 }
-// end employees
+
+
+// start questions 
+  const questions = document.querySelectorAll('.questions__item')
+  const answer = document.querySelectorAll('.questions__item_answer')
+  console.log(questions)
+  questions.forEach(function(question, i) {
+    question.addEventListener('click', function (e) {
+      answer[i].classList.toggle('active')
+    })
+  })
+// end questions 
